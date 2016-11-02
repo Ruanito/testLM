cf4ddeeb2a9fc4a0b7d251b147992ef4a4d1d75c
Ruanito Santos <ruanitodiego@gmail.com>
2016-11-02 21:44:50 -0200

    Add test to spearkers

--- spec/spec_macbeth.rb
+++ spec/spec_macbeth.rb
@@ -8,4 +8,22 @@ RSpec.describe MacbethAnalyser, "Read File" do
 			expect(macbeth.source_location).to eq 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'
 		end
 	end
+
+	context "Speakers"
+		it "Hash new" do
+			macbeth = MacbethAnalyser.new
+			expect(macbeth.speakers).to eq Hash.new
+		end
+
+		it "Hash DUNCAN nil" do
+			macbeth = MacbethAnalyser.new
+			expect(macbeth.speakers['DUNCAN']).to eq nil
+		end
+
+		it "Hash DUNCAN count" do
+			macbeth = MacbethAnalyser.new
+			macbeth.xml_parse
+			expect(macbeth.speakers['DUNCAN']).to exist
+		end
+	end
 end
