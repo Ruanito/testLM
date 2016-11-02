7b4c72fcbe7c139cef8c364a9642b2281b137e38
Ruanito Santos <ruanitodiego@gmail.com>
2016-11-02 17:37:34 -0200

    Add XML source and nokogiri require

--- lib/macbeth_analyser.rb
+++ lib/macbeth_analyser.rb
@@ -1,2 +1,11 @@
+require 'nokogiri'
+
 class MacbethAnalyser
+
+	XML_SOURCE = 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'
+
+	def initialize
+		file = File.read(open(XML_SOURCE))
+	end
+
 end
