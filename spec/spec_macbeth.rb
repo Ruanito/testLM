70f427750b9522db788b13a1fc6ec31ddb781dda
Ruanito Santos <ruanitodiego@gmail.com>
2016-11-03 08:43:53 -0200

    Fixed name

--- spec/spec_macbeth.rb
+++ spec/spec_macbeth.rb
@@ -15,15 +15,15 @@ RSpec.describe MacbethAnalyser, "Read File" do
 			expect(macbeth.speakers).to eq Hash.new
 		end
 
-		it "Hash DUNCAN nil" do
+		it "Hash Duncan nil" do
 			macbeth = MacbethAnalyser.new('macbeth.xml')
-			expect(macbeth.speakers['DUNCAN']).to eq nil
+			expect(macbeth.speakers['Duncan']).to eq nil
 		end
 
-		it "Hash DUNCAN count" do
+		it "Hash Duncan count" do
 			macbeth = MacbethAnalyser.new('macbeth.xml')
 			macbeth.xml_parse
-			expect(macbeth.speakers['DUNCAN']).to exist
+			expect(macbeth.speakers['Duncan']).to exist
 		end
 	end
 end
